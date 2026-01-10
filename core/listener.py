eb56d9b158ee9efbbde8e230aded31ef82999efe
Md Kamran Akhter <mdkamranakhter78@gmail.com>
2026-01-10 23:59:10 +0530

    added offline support

--- core/listener.py
+++ core/listener.py
@@ -1,20 +1,51 @@
-import speech_recognition as sr
+import queue
+import sounddevice as sd
+import socket
+import json
+import vosk
 
-recognizer = sr.Recognizer()
+vosk.SetLogLevel(-1)
+model = vosk.Model("models/vosk-model-small-en-us-0.15")
+q = queue.Queue()
+
+def is_connected():
+    try:
+        socket.create_connection(("8.8.8.8", 53), timeout=2)
+        return True
+    except OSError:
+        return False
+
+def callback(indata, frames, time, status):
+    if status:
+        print(status)
+    q.put(bytes(indata))
 
 def listen():
-    with sr.Microphone() as source:
-        print("Listening...")
-        recognizer.adjust_for_ambient_noise(source, duration=0.5)
-        audio = recognizer.listen(source)
+    if is_connected():
+        import speech_recognition as sr
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Listening(online)...")
+            r.adjust_for_ambient_noise(source, duration=0.5)
+            audio = r.listen(source)
     
-    try:
-        text = recognizer.recognize_google(audio)
-        print("You said:", text)
-        return text.lower()
+        try:
+            text = r.recognize_google(audio)
+            print("Online:", text)
+            return text.lower()
+        except:
+            return ""
+    else:
+        print("Listening (offline)...")
+        rec = vosk.KaldiRecognizer(model, 16000)
 
-    except sr.UnknownValueError:
-        return ""
-
-    except sr.RequestError:
-        return "internet_error"
+        with sd.RawInputStream(samplerate=16000, blocksize=8000, dtype='int16',
+                               channels=1, callback=callback):
+            while True:
+                data = q.get()
+                if rec.AcceptWaveform(data):
+                    result= json.loads(rec.Result())
+                    text = result.get("text", "").strip()
+                    if text:
+                        print("Offline:", text)
+                        return text.lower()
--- core/router.py
+++ core/router.py
@@ -2,33 +2,44 @@ from core.speaker import speak
 import datetime
 import os
 import webbrowser
+import difflib
+
+def match(words, text):
+    for word in words:
+        if word in text:
+            return True
+        
+        ratio = difflib.SequenceMatcher(None, word, text). ratio()
+        if ratio > 0.6:
+            return True
+    return False
 
 def handle_command(command):
     if not command:
         return
-    
-    if "time" in command:
+
+    if match(["time", "clock"], command):
         now = datetime.datetime.now().strftime("%I:%M %p")
         speak(f"The time is {now}")
 
-    elif "date" in command:
+    elif match(["date", "today"], command):
         today = datetime.datetime.now().strftime("%B %d, %Y")
         speak(f"Today's date is {today}")
 
-    elif "open notepad" in command:
+    elif match(["notepad", "notes"], command):
         speak("Opening Notepad")
         os.system("notepad")
 
-    elif "open calculator" in command:
+    elif match(["calculator", "calc"], command):
         speak("Opening Calculator")
         os.system("calc")
 
-    elif "open youtube" in command:
+    elif match(["youtube", "video"], command):
         speak("Opening YouTube")
         webbrowser.open("https://youtube.com")
 
-    elif "hello" in command or "hi" in command:
-        speak("Hello! How can I help you")
+    elif match(["hello", "hi", "hey"], command):
+        speak("Hello! How can I help you?")
 
     else:
         speak("I didn't understand that.")
